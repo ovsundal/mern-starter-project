be968b4a5fbad6fd481545a7529c80554e0de7ab
osund <osundal83@gmail.com>
2020-02-03 22:42:46 +0100

    add drawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,24 +1,42 @@
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import BackDrop from '../UiElements/Backdrop'
 
 const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => setDrawerIsOpen(true);
+
+  const closeDrawer = () => setDrawerIsOpen(false);
+
   return (
-    <MainHeader>
-      <button className={'main-navigation__menu-btn'}>
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className={"main-navigation__title"}>
-        <Link to={"/"}>YourPlaces</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+        {drawerIsOpen && <BackDrop onClick={closeDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className={"main-navigation__drawer-nav"}>
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button className={"main-navigation__menu-btn"} onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className={"main-navigation__title"}>
+          <Link to={"/"}>YourPlaces</Link>
+        </h1>
+        <nav className={"main-navigation__header-nav"}>
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,6 +1,7 @@
 import "./UsersList.css";
 import React from "react";
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UiElements/Card";
 
 const UsersList = props => {
   console.log(props);
@@ -8,13 +9,15 @@ const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className={"center"}>
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
 
   return (
-    <ul className={'users-list'}>
+    <ul className={"users-list"}>
       {props.items.map(user => {
         return (
           <UserItem
