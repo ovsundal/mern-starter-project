171c454d152fefc183cbb210f1e25aae27abfa76
osund <osundal83@gmail.com>
2020-02-04 22:03:01 +0100

    add styled buttons

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,37 +1,39 @@
 import PlaceList from "../components/PlaceList";
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world',
-        imageUrl: "https://via.placeholder.com/150",
-        address: '20 W 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u1'
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    imageUrl: "https://via.placeholder.com/150",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
     },
-    {
-        id: 'p2',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world',
-        imageUrl: "https://via.placeholder.com/150",
-        address: '20 W 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u2'
-    }
+    creator: "u1"
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    imageUrl: "https://via.placeholder.com/150",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: "u2"
+  }
 ];
 
 const UserPlaces = () => {
-    return (
-        <PlaceList items={DUMMY_PLACES} />
-    )
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
