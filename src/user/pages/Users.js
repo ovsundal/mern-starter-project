7145cf98a08afd902fb92085829451713cc6463b
ove <osundal83@gmail.com>
2020-02-28 14:15:10 +0100

    connect getUsers to endpoint

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
+import LoadingSpinner from "../../shared/components/UiElements/LoadingSpinner";
+import ErrorModal from "../../shared/components/UiElements/ErrorModal";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Max Schwarz",
-      image: "https://via.placeholder.com/150",
-      places: 3
-    }
-  ];
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-  return <UsersList items={USERS} />;
+  useEffect(() => {
+    const sendRequest = async () => {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch("http://localhost:5000/users");
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setLoadedUsers(responseData.users);
+        setIsLoading(false);
+      } catch (e) {
+        setIsLoading(false);
+        setError(e.message);
+      }
+    };
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className={"center"}>
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
 };
 
 export default Users;
